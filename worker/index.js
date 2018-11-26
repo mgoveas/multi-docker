aeab3636737b6c5dced5f87ad7043dffbbbef1fa
Maria Goveas <Maria_Goveas@intuit.com>
2018-11-25 23:02:25 -0800

    Initial commit

---
+++ worker/index.js
@@ -0,0 +1,24 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();
+sub.subscribe('insert');
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('error', function(err){
+  console.log('Something went wrong ', err)
+});
+
+sub.on('message', (channel, message) => {
+  console.log("i am here");
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
